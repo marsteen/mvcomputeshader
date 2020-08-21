c7e69fa1d3a650afc7d71dde6abbba0f18216fcf
Martin Steen <martin.steen@ksti.de>
2020-08-21 13:32:47 +0200

    initial commit

---
+++ source/CShaderTool.h
@@ -0,0 +1,26 @@
+
+
+#ifndef CShaderTool_H
+#define CShaderTool_H
+
+#include <GL/glew.h>
+
+class CShaderTool
+{
+	public:
+	
+	    void init();
+		void updateTex(int frame);
+		void draw();
+		static void checkErrors(std::string desc);
+		
+	protected:
+		
+		static GLuint genRenderProg(GLuint texHandle);
+		static GLuint genComputeProg(GLuint texHandle);
+		static GLuint genTexture();
+	
+		GLuint renderHandle, computeHandle, texHandle;
+};
+
+#endif
---
+++ source/main.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <CShaderTool.h>
+
+
+using namespace std;
+
+int main() 
+{
+	
+	if (!glfwInit())
+	{
+        return -1;
+	}
+	
+	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello computeshader world", NULL, NULL);
+	if (window == nullptr)
+	{
+		return -1;
+	}
+	
+	cout << "glfwCreateWindow ok" << endl;
+	
+	glfwMakeContextCurrent(window);
+	glewInit();
+
+    CShaderTool* ShaderTool = new CShaderTool;
+	
+	ShaderTool->init();
+	
+	cout << "ShaderTool init ok" << endl;
+	
+	int frame = 0;
+	
+	while (!glfwWindowShouldClose(window))
+	{
+		ShaderTool->updateTex(frame++);
+		ShaderTool->draw();
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	return 0;
+}
+
